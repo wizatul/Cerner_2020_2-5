146384a417554b6b45320868695449b9ff58fee4
Dey <AD065110@Cerner.net>
2020-09-27 21:10:52 +0530

    New: commit stateSingleton.go

---
+++ stateSingleton.go
@@ -0,0 +1,20 @@
+package stateSingleton
+//Always Go for singleton
+type manager struct {
+    state string
+}
+// cerner_2^5_2020
+var singleton *manager
+var once sync.Once
+func GetManager() *manager {
+    once.Do(func() {
+        singleton = &manager{state: "off"}
+    })
+    return singleton
+}
+func (sm *manager) GetState() string {
+    return sm.state
+}
+func (sm *manager) SetState(s string) {
+    sm.state = s
+}

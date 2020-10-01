a4eb84603920710b3aa4b2c4f69be2dd9aef24ae
ATul Dey <atuldey1@gmail.com>
2020-10-01 16:20:30 +0530

    New: commit SingletonCPP.cpp

---
+++ SingletonCPP.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//Program for cerner_2^5_2020 
+using namespace std;
+class SingletonCPP {
+   static SingletonCPP *instance;
+   int data;
+   // Private constructor so that no objects can be created.
+   SingletonCPP() {
+      data = 0;
+   }
+   // cerner_2^5_2020 
+   public:
+   static SingletonCPP *getInstance() {
+      if (!instance)
+      instance = new SingletonCPP;
+      return instance;
+   }
+   int getData() {
+      return this -> data;
+   }
+   void setData(int data) {
+      this -> data = data;
+   }
+};
+//Initialize pointer to zero so that it can be initialized in first call to getInstance
+SingletonCPP *SingletonCPP::instance = 0;
+int main(){
+   SingletonCPP *s = s->getInstance();
+   cout << s->getData() << endl;
+   s->setData(100);
+   cout << s->getData() << endl;
+   return 0;
+}
